3fa85f173e80a7fd34b6f6fb0177afecc1c8ad24
pradosavljevic <pradosavljevic@student.tgm.ac.at>
2017-03-29 13:37:43 +0200

    Commit 3

--- src/petar/hallo.java
+++ src/petar/hallo.java
@@ -4,7 +4,7 @@ public class hallo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo petar!");
+		System.out.println("Hallo petar :-)");
 
 	}
 
